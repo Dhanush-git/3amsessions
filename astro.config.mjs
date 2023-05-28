50f9a2ff04f5f061c655e4c91bce18868304f3bd
Dhanush Suvarna <dhanush@marmeto.com>
2023-05-28 23:51:41 +0530

    added github workflow for astro deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://Dhanush-git.github.io',
+  base: '/3amsessions',
   integrations: [tailwind()]
 });
