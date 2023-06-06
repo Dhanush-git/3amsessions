d1e9c0f36ac13afbeb5647346144aab598c19b8c
Dhanush Suvarna <dhanush@marmeto.com>
2023-06-06 09:59:55 +0530

    Changed base

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://Dhanush-git.github.io',
-  base: '/3amsessions',
+  base: '/',
   integrations: [tailwind()]
 });
